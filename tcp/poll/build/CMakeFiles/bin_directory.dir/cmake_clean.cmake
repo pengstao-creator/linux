1407de842c755220fd8a01317e5cf0825f4d58f4
别动我的肠 <120660082@qq.com>
2026-03-18 23:38:20 +0800

    Remove binary and build files from git

--- tcp/poll/build/CMakeFiles/bin_directory.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/bin_directory"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/bin_directory.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
